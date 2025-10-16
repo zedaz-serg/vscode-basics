5a323380239115783ebf148c43162568d8588056
Serg_Zedaz <zedazen-git@gmail.com>
2025-10-16 21:11:33 +0300

    first commit

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-print("Hello World!")
-
-print("Hello Git")
+def format_string(string:str, length:int):
+    if len(string) >= length:
+        return string
+    else:
+        spaces = (length - len(string)) // 2
+        string = " " * spaces + string
+        return string
+format_string("aaaa",10)
